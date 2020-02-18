a01a16a0808fc950d5e4bfd80d74bf91264e8a62
cdfive <cdfive@qq.com>
2020-02-18 22:35:03 +0800

    Add custom exception handler.

---
+++ src/main/java/com/cdfive/learning/gateway/exception/GatewayExceptionHandler.java
@@ -0,0 +1,119 @@
+package com.cdfive.learning.gateway.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.cloud.gateway.support.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.HttpMessageReader;
+import org.springframework.http.codec.HttpMessageWriter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.util.Assert;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.result.view.ViewResolver;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author cdfive
+ */
+public class GatewayExceptionHandler implements ErrorWebExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GatewayExceptionHandler.class);
+
+    private List<ViewResolver> viewResolvers = Collections.emptyList();
+
+    private List<HttpMessageReader<?>> messageReaders = Collections.emptyList();
+
+    private List<HttpMessageWriter<?>> messageWriters = Collections.emptyList();
+
+    private ThreadLocal<Map<String, Object>> exceptionHandlerResult = new ThreadLocal<>();
+
+    public void setViewResolvers(List<ViewResolver> viewResolvers) {
+        this.viewResolvers = viewResolvers;
+    }
+
+    public void setMessageReaders(List<HttpMessageReader<?>> messageReaders) {
+        Assert.notNull(messageReaders, "messageReaders must not be null");
+        this.messageReaders = messageReaders;
+    }
+
+    public void setMessageWriters(List<HttpMessageWriter<?>> messageWriters) {
+        Assert.notNull(messageWriters, "messageWriters must not be null");
+        this.messageWriters = messageWriters;
+    }
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+        HttpStatus httpStatus;
+        String body;
+        if (ex instanceof NotFoundException) {
+            httpStatus = HttpStatus.NOT_FOUND;
+            body = "Service Not Found";
+        } else if (ex instanceof ResponseStatusException) {
+            ResponseStatusException responseStatusException = (ResponseStatusException) ex;
+            httpStatus = responseStatusException.getStatus();
+            body = responseStatusException.getMessage();
+        } else {
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+            body = "Internal Server Error";
+        }
+
+        Map<String, Object> result = new HashMap<>(2, 1);
+        result.put("httpStatus", httpStatus);
+        result.put("body", body);
+
+        ServerHttpRequest request = exchange.getRequest();
+        log.error("[GatewayExceptionHandler]path={},message={}", request.getPath().value(), ex.getMessage());
+
+        if (exchange.getResponse().isCommitted()) {
+            return Mono.error(ex);
+        }
+
+        exceptionHandlerResult.set(result);
+        ServerRequest newRequest = ServerRequest.create(exchange, this.messageReaders);
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse).route(newRequest)
+                .switchIfEmpty(Mono.error(ex))
+                .flatMap((handler) -> handler.handle(newRequest))
+                .flatMap((response) -> write(exchange, response));
+    }
+
+    protected Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
+        Map<String, Object> result = exceptionHandlerResult.get();
+        return ServerResponse.status((HttpStatus) result.get("httpStatus"))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(BodyInserters.fromObject(result.get("body")));
+    }
+
+    private Mono<? extends Void> write(ServerWebExchange exchange,
+                                       ServerResponse response) {
+        exchange.getResponse().getHeaders()
+                .setContentType(response.headers().getContentType());
+        return response.writeTo(exchange, new ResponseContext());
+    }
+
+    private class ResponseContext implements ServerResponse.Context {
+
+        @Override
+        public List<HttpMessageWriter<?>> messageWriters() {
+            return GatewayExceptionHandler.this.messageWriters;
+        }
+
+        @Override
+        public List<ViewResolver> viewResolvers() {
+            return GatewayExceptionHandler.this.viewResolvers;
+        }
+
+    }
+}
